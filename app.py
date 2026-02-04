5e7bc3daa384f54385ddad59fefdf2f789d66dcd
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2026-02-04 02:16:45 -0700

    tweaks  -- this is a bad commit message

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
 
 
 @app.route('/')
